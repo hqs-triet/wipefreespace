c2db2e7573afeb03b48cd0af57b7a4d639333ce5
Atom <hqs.triet@gmail.com>
2024-02-17 13:11:44 +0700

    Version 1.0

---
+++ WipeFreeSpace/FormMain.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace WipeFreeSpace
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBaseDir = new System.Windows.Forms.TextBox();
+            this.btnBrowseFolder = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressMain = new System.Windows.Forms.ProgressBar();
+            this.selectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(223, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Base directory:";
+            // 
+            // txtBaseDir
+            // 
+            this.txtBaseDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBaseDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBaseDir.Location = new System.Drawing.Point(364, 9);
+            this.txtBaseDir.Name = "txtBaseDir";
+            this.txtBaseDir.Size = new System.Drawing.Size(289, 24);
+            this.txtBaseDir.TabIndex = 2;
+            this.txtBaseDir.Text = "D:\\temp\\Test";
+            // 
+            // btnBrowseFolder
+            // 
+            this.btnBrowseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowseFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseFolder.Location = new System.Drawing.Point(657, 9);
+            this.btnBrowseFolder.Name = "btnBrowseFolder";
+            this.btnBrowseFolder.Size = new System.Drawing.Size(35, 24);
+            this.btnBrowseFolder.TabIndex = 3;
+            this.btnBrowseFolder.Text = "...";
+            this.btnBrowseFolder.UseVisualStyleBackColor = true;
+            this.btnBrowseFolder.Click += new System.EventHandler(this.btnBrowseFolder_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFileName.Location = new System.Drawing.Point(364, 50);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(132, 24);
+            this.txtFileName.TabIndex = 5;
+            this.txtFileName.Text = "wipe.bin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(251, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "File name:";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(98)))), ((int)(((byte)(161)))));
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.ForeColor = System.Drawing.Color.White;
+            this.btnStop.Location = new System.Drawing.Point(472, 269);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(101, 37);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(251, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Write size";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(361, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Write speed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(478, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 18);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Eslapsed time";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.Location = new System.Drawing.Point(251, 153);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(62, 18);
+            this.lblSize.TabIndex = 14;
+            this.lblSize.Text = "File size";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpeed.Location = new System.Drawing.Point(361, 153);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(41, 18);
+            this.lblSpeed.TabIndex = 15;
+            this.lblSpeed.Text = "Mb/s";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(496, 153);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(64, 18);
+            this.lblTime.TabIndex = 16;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(98)))), ((int)(((byte)(161)))));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(591, 269);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 37);
+            this.btnStart.TabIndex = 17;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // progressMain
+            // 
+            this.progressMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressMain.Location = new System.Drawing.Point(254, 194);
+            this.progressMain.MarqueeAnimationSpeed = 50;
+            this.progressMain.Name = "progressMain";
+            this.progressMain.Size = new System.Drawing.Size(438, 31);
+            this.progressMain.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressMain.TabIndex = 18;
+            this.progressMain.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Location = new System.Drawing.Point(12, 261);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(680, 2);
+            this.panel1.TabIndex = 19;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(66, 228);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(144, 18);
+            this.linkLabel1.TabIndex = 21;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "hqs.triet@gmail.com";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 18);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Author:";
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.AutoSize = true;
+            this.lblTimeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeLeft.Location = new System.Drawing.Point(603, 153);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(64, 18);
+            this.lblTimeLeft.TabIndex = 23;
+            this.lblTimeLeft.Text = "00:00:00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(603, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 18);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Time left";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 318);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressMain);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseFolder);
+            this.Controls.Add(this.txtBaseDir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Wipe free space of disk";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBaseDir;
+        private System.Windows.Forms.Button btnBrowseFolder;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressMain;
+        private System.Windows.Forms.FolderBrowserDialog selectFolder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WipeFreeSpace/FormMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WipeFreeSpace
+{
+    public partial class FormMain : Form
+    {
+        private bool m_stop = true;
+        const int BLOCK_SIZE = 256;
+        DriveInfo m_drive = null;
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFolder_Click(object sender, EventArgs e)
+        {
+            if(this.selectFolder.ShowDialog() == DialogResult.OK)
+            {
+                this.txtBaseDir.Text = this.selectFolder.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(!Directory.Exists(this.txtBaseDir.Text))
+            {
+                MessageBox.Show("The base directory does not exist!");
+                return;
+            }
+            if(string.IsNullOrEmpty(this.txtFileName.Text))
+            {
+                MessageBox.Show("The file name is empty!");
+                return;
+            }
+            string fFullPath = Path.Combine(this.txtBaseDir.Text, this.txtFileName.Text);
+            if (File.Exists(fFullPath))
+                if (MessageBox.Show("The target file existed. Do you want to overwrite?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.No)
+                    return;
+
+            FileInfo f = new FileInfo(fFullPath);
+            m_drive = new DriveInfo(Path.GetPathRoot(f.FullName));
+
+            Thread thr = new Thread(new ThreadStart(() =>
+            {
+                byte[] arrData = GenerateBlock();
+                DateTime timeStart = DateTime.Now;
+                double totalSize = 0, sizeCountInOneSecond = 0, totalWrite = 0;
+                try
+                {
+                    using (var fs = new FileStream(fFullPath, FileMode.Create))
+                    {
+                        DateTime timeCountStart = DateTime.Now;
+                        while (!m_stop)
+                        {
+                            fs.Write(arrData, 0, BLOCK_SIZE);
+                            totalWrite++;
+                            totalSize += BLOCK_SIZE;
+                            sizeCountInOneSecond += BLOCK_SIZE;
+
+                            // Update parameters
+                            if (DateTime.Now.Subtract(timeCountStart).TotalSeconds >= 1)
+                            {
+                                //Console.WriteLine("================= Total write:  " + totalWrite);
+                                //Console.WriteLine("================= sizeCountInOneSecond:  " + sizeCountInOneSecond);
+                                Invoke(new Action(() =>
+                                {
+                                    this.lblSize.Text = MakeLabelSize(totalSize);
+                                    this.lblSpeed.Text = MakeLabelSpeed(sizeCountInOneSecond);
+
+                                    TimeSpan diffTime = DateTime.Now.Subtract(timeStart);
+                                    this.lblTime.Text = diffTime.Hours.ToString().PadLeft(2, '0') + ":" +
+                                                        diffTime.Minutes.ToString().PadLeft(2, '0') + ":" +
+                                                        diffTime.Seconds.ToString().PadLeft(2, '0');
+                                    if (sizeCountInOneSecond > 0)
+                                    {
+                                        long totalSeconds = (long)(m_drive.TotalFreeSpace / sizeCountInOneSecond);
+                                        var tsTimeLeft = TimeSpan.FromSeconds(totalSeconds);
+                                        this.lblTimeLeft.Text = tsTimeLeft.Hours.ToString().PadLeft(2, '0') + ":" +
+                                                            tsTimeLeft.Minutes.ToString().PadLeft(2, '0') + ":" +
+                                                            tsTimeLeft.Seconds.ToString().PadLeft(2, '0');
+                                    }
+
+                                }));
+                                sizeCountInOneSecond = 0;
+                                timeCountStart = DateTime.Now;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (m_drive.TotalFreeSpace < BLOCK_SIZE)
+                        Invoke(new Action(() =>
+                        {
+                            MessageBox.Show("Done!");
+                        }));
+                    else
+                        Invoke(new Action(() =>
+                        {
+                            MessageBox.Show(ex.Message);
+                        }));
+
+                }
+
+                Invoke(new Action(() =>
+                {
+                    this.btnStart.Enabled = true;
+                    this.btnStop.Enabled = false;
+                    this.progressMain.Visible = false;
+                    this.txtBaseDir.Enabled = true;
+                    this.txtFileName.Enabled = true;
+                    this.btnBrowseFolder.Enabled = true;
+                }));
+            }));
+            m_stop = false;
+            this.btnStart.Enabled = false;
+            this.btnStop.Enabled = true;
+            this.progressMain.Visible = true;
+            this.txtBaseDir.Enabled = false;
+            this.txtFileName.Enabled = false;
+            this.btnBrowseFolder.Enabled = false;
+            thr.Start();
+        }
+        private string MakeLabelSpeed(double count)
+        {
+            if (count < 1024)
+                return count + " b/s";
+            
+            if (count < 1024 * 1024)
+                return (count / 1024).ToString("0.00") + " kb/s";
+
+            if (count < 1024 * 1024 * 1024)
+                return (count / 1024 / 1024).ToString("0.00") + " mb/s";
+            
+            if (count < (double)1024 * 1024 * 1024 * 1024)
+                return (count / 1024 / 1024 / 1024).ToString("0.00") + " gb/s";
+            
+            return "" + count;
+        }
+        private string MakeLabelSize(double size)
+        {
+            if (size < 1024)
+                return size + " byte(s)";
+
+            if (size < 1024 * 1024)
+                return (size / 1024).ToString("0.00") + " kb(s)";
+
+            if (size < 1024 * 1024 * 1024)
+                return (size / 1024 / 1024).ToString("0.00") + " mb(s)";
+
+            if (size < (double)1024 * 1024 * 1024 * 1024)
+                return (size / 1024 / 1024 / 1024).ToString("0.00") + " gb(s)";
+
+            return "";
+        }
+        private byte[] GenerateBlock()
+        {
+            byte[] arrData = new byte[BLOCK_SIZE];
+            for(int idx = 0; idx < BLOCK_SIZE; idx++)
+            {
+                arrData[idx] = 0;
+            }
+            return arrData;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.lblSize.Text = string.Empty;
+            this.lblSpeed.Text = string.Empty;
+            this.lblTime.Text = string.Empty;
+            this.progressMain.Visible = false;
+            this.lblTimeLeft.Text = string.Empty;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            m_stop = true;
+        }
+    }
+}
